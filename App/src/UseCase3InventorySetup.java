7f701e1743e4fd34b4d88f30ae6b76dea01e852f
10pranjal2005 <pk5518@srmist.edu.in>
2026-03-13 18:33:14 +0530

    Pranjal Added UC3

---
+++ App/src/UseCase3InventorySetup.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/* Inventory manager responsible for room availability */
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int newCount) {
+        if (inventory.containsKey(roomType)) {
+            inventory.put(roomType, newCount);
+        }
+    }
+
+    public void displayInventory() {
+        System.out.println("===== Current Room Inventory =====");
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
+
+/* Application entry point for UC3 */
+
+public class UseCase3InventorySetup {
+
+    public static void main(String[] args) {
+
+        RoomInventory inventory = new RoomInventory();
+
+        inventory.displayInventory();
+
+        System.out.println("\nUpdating inventory...\n");
+
+        inventory.updateAvailability("Single Room", 4);
+
+        inventory.displayInventory();
+    }
+}
